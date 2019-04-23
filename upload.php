5b8ec93305e6398b883302ebc719e2d6e5d842a0
AddiJack <44784763+AddiJack@users.noreply.github.com>
2019-04-23 11:19:03 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,9 +1,7 @@
 <?php
-
 if (isset($_POST['submit'])){
     if (count($_FILES['fichier']['name'])>0){
         for($i=0; $i<count($_FILES['fichier']['name']); $i++) {
-
     $fileTmpName = $_FILES['fichier']['tmp_name'][$i];
             
             $filename = $_FILES['fichier']['name'][$i];
@@ -13,25 +11,20 @@ if (isset($_POST['submit'])){
             $fileExt = explode('.', $filename);
             $fileActualExt = strtolower(end($fileExt));
             $allowed = array('jpg','png','gif');
-
     if (in_array($fileActualExt, $allowed)){
         
         if ($fileError === 0){
-
             if($fileSize <= 1000000){
-                $fileNameNew = uniqid('', true).".".$fileActualExt;
+                $fileNameNew = uniqid('image', true).".".$fileActualExt;
                 $fileDestination = 'uploads/'.$fileNameNew;
     move_uploaded_file($fileTmpName, $fileDestination);
     header('Location: index.php');
-
             } else {
                 echo "Fichier trop volumineux.";
             }
-
         }else{
             "Il y a une erreur de téléchargement.";
         }
-
     } else {
         echo "Le type de fichier n'est pas bon.";
     }
