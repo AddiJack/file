4c71fae5c6550e377ae92dd148fb6b702f0f752f
AddiJack <44784763+AddiJack@users.noreply.github.com>
2019-04-23 11:12:16 +0200

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,40 @@
+<?php
+
+if (isset($_POST['submit'])){
+    if (count($_FILES['fichier']['name'])>0){
+        for($i=0; $i<count($_FILES['fichier']['name']); $i++) {
+
+    $fileTmpName = $_FILES['fichier']['tmp_name'][$i];
+            
+            $filename = $_FILES['fichier']['name'][$i];
+            $fileType = $_FILES['fichier']['type'][$i];
+            $fileSize = $_FILES['fichier']['size'][$i];
+            $fileError = $_FILES['fichier']['error'][$i];
+            $fileExt = explode('.', $filename);
+            $fileActualExt = strtolower(end($fileExt));
+            $allowed = array('jpg','png','gif');
+
+    if (in_array($fileActualExt, $allowed)){
+        
+        if ($fileError === 0){
+
+            if($fileSize <= 1000000){
+                $fileNameNew = uniqid('', true).".".$fileActualExt;
+                $fileDestination = 'uploads/'.$fileNameNew;
+    move_uploaded_file($fileTmpName, $fileDestination);
+    header('Location: index.php');
+
+            } else {
+                echo "Fichier trop volumineux.";
+            }
+
+        }else{
+            "Il y a une erreur de téléchargement.";
+        }
+
+    } else {
+        echo "Le type de fichier n'est pas bon.";
+    }
+}
+    }
+}
